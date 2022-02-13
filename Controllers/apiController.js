a11d1748457675a52497f3c6a5e785ba97324bb9
Duong <Duongchauv@gmail.com>
2022-02-13 17:35:52 -0600

    Finished all basic create/read routes without proper form validation

--- Controllers/apiController.js
+++ Controllers/apiController.js
@@ -3,6 +3,26 @@ const {body,param,validationResult} = require('express-validator')
 const pool = require('../DatabaseConfig/mysql_config')
 const jwt = require('express-jwt');
 
+
+/* 
+    CREATE PROJECT - X
+    UPDATE PROJECT - USER/ADMIN ONLY
+    DELETE PROJECT - USER/ADMIN ONLY
+    GET PROJECTS - X
+    GET SINGLE PROJECT - X
+
+    CREATE BUG - X
+    UPDATE BUG - USER/ADMIN ONLY
+    DELETE BUG - USER/ADMIN ONLY
+    GET BUGS - X
+    GET SINGLE BUG - X
+ 
+    CREATE COMMENT - X
+    UPDATE COMMENT - USER/ADMIN ONLY
+    DELETE COMMENT - USER/ADMIN ONLY
+    GET COMMENTS - X
+    GET SINGLE COMMENT - X
+*/
 function checkExist(db, name, connection){
     return new Promise((resolve,reject)=>{
         let queryStr = `SELECT * FROM ${db} WHERE name = ?`;
@@ -27,6 +47,40 @@ function createNewProject(userId, name, description,connection){
     })
 }
 
+function createNewBug(description,status,severity,name,user_id,project_id,connection){
+    return new Promise((resolve,reject)=>{
+        let queryStr = "INSERT INTO bugs(bug_date,description,status,severity,name,user_id,project_id) VALUES(NOW(), ?, ?, ?, ?, ?, ?)";
+        let query = mysql.format(queryStr, [description,status,severity,name,user_id,project_id])
+
+        connection.query(query, function(err,results){
+            if(err) return reject(err);
+            return resolve(results);
+        })
+    })
+}
+
+exports.getProjects = [
+    async function(req,res,next){
+        let queryStr = `SELECT * FROM projects`
+        let query = mysql.format(queryStr)
+
+        let results = await pool.query(query)
+        res.json(results)
+    }
+]
+
+exports.getSingleProject = [
+    async function(req,res,next){
+        const {project_id} = req.params;
+
+        let queryStr = `SELECT * FROM projects where id = ?`
+        let query = mysql.format(queryStr, [project_id])
+
+        let results = await pool.query(query)
+        res.json(results)
+    }
+]
+
 exports.createProject=[
     jwt({
         secret:process.env.TOKEN_SECRET,
@@ -39,13 +93,13 @@ exports.createProject=[
         const {id} = req.user.signedUser;
         pool.getConnection( async (err,connection)=>{
             try{
-                let projectExist = await checkExist('projects', name, connection);
+                let projectExist = await checkExist('bugs', name, connection);
                 if(projectExist.length!=0){
                     res.send('Project exists already')
                 }
                 else{
                     let projectCreate = await createNewProject(id,name,description,connection)
-                    console.log(projectCreate);
+                    console.log(projectCreate); //
                     res.send('Should work')
                 }
             }
@@ -59,6 +113,31 @@ exports.createProject=[
     }
 ]
 
+exports.getBugs=[
+    async function(req,res,next){
+        const {project_id} = req.params;
+
+        let queryStr = `SELECT * FROM bugs WHERE project_id = ?`
+        let query = mysql.format(queryStr, [project_id])
+
+        let results = await pool.query(query)
+        res.json(results)
+
+    }
+]
+
+exports.getSingleBug = [
+    async function(req,res,next){
+        const {bug_id, project_id} = req.params;
+
+        let queryStr = `SELECT * FROM bugs where id = ? AND project_id = ?`
+        let query = mysql.format(queryStr, [bug_id, project_id])
+
+        let results = await pool.query(query)
+        res.json(results)
+    }
+]
+
 exports.createBug=[
     jwt({
         secret:process.env.TOKEN_SECRET,
@@ -67,19 +146,73 @@ exports.createBug=[
         credentialsRequired:true,
     }),
     async function(req,res,next){
-        const {name, description, project_id} = req.body;
+        const {name, description, status, severity} = req.body;
+        const {project_id} = req.params;
         const {id} = req.user.signedUser;
         pool.getConnection( async (err,connection)=>{
             try{
-                let projectExist = await checkExist('projects', name, connection);
-                if(projectExist.length!=0){
-                    res.send('Project exists already')
+                let bugExist = await checkExist('bugs', name, connection);
+                if(bugExist.length!=0){
+                    res.send('Bug exists already')
                 }
                 else{
-                    let projectCreate = await createNewProject(id,name,description,connection)
-                    console.log(projectCreate);
+                    let bugCreate = await createNewBug(description,status,severity,name,id,project_id,connection)
+                    console.log(bugCreate); //
                     res.send('Should work')
                 }
+            }
+            catch(err){
+                res.send(err)
+            }
+            finally{
+                connection.release();
+            }
+        })
+    }
+]
+
+exports.getComments = [
+    async function(req,res,next){
+        const {bug_id} = req.params;
+
+        let queryStr = `SELECT * FROM comments WHERE bug_id = ?`
+        let query = mysql.format(queryStr, [bug_id])
+
+        let results = await pool.query(query)
+        res.json(results)
+    }
+]
+
+exports.getSingleComment = [
+    async function(req,res,next){
+        const {bug_id, project_id, comment_id} = req.params;
+
+        let queryStr = `SELECT * FROM comments WHERE id =? AND bug_id = ? AND project_id = ?`
+        let query = mysql.format(queryStr, [comment_id, bug_id, project_id])
+
+        let results = await pool.query(query)
+        res.json(results)
+    }
+]
+
+exports.createComment = [
+    jwt({
+        secret:process.env.TOKEN_SECRET,
+        algorithms: ['HS256'],
+        getToken: req => req.cookies.token,
+        credentialsRequired:true,
+    }),
+    async function(req,res,next){
+        const {name, description} = req.body;
+        const {bug_id, project_id} = req.params;
+        const {id} = req.user.signedUser;
+        pool.getConnection( async (err,connection)=>{
+            try{
+                let queryStr = `INSERT INTO comments(user_id, bug_id, project_id, description, comment_date, name) VALUES(?, ?, ?, ?, NOW(), ?)`;
+                let query = mysql.format(queryStr, [id,bug_id,project_id,description,name]);
+
+                let results = await pool.query(query) 
+                res.json(results)
             }
             catch(err){
                 res.send(err)
--- routes/api.js
+++ routes/api.js
@@ -6,10 +6,23 @@ router.get('/',(req,res)=>{
     res.send('hi')
 })
 
-router.post('/project', apiController.createProject)
-// router.get('/bug', apiController.getBugs)
-router.post('/bug', apiController.createBug )
-// router.put('/bug', )
-// router.delete('/bug', )
+router.get('/project', apiController.getProjects) //Get All Projects
+router.post('/project', apiController.createProject) //Create a single project
+
+router.get('/project/:project_id', apiController.getSingleProject) //Get single project
+//Update Single Project
+//Delete Single Project
+
+router.get('/project/:project_id/bug', apiController.getBugs ) //Get all Bugs
+router.post('/project/:project_id/bug', apiController.createBug ) //Create single bug
+router.get('/project/:project_id/bug/:bug_id', apiController.getSingleBug)
+//Update Single Bug
+//Delete Single Bug
+
+router.get('/project/:project_id/bug/:bug_id/comment', apiController.getComments)//Get all comments
+router.post('/project/:project_id/bug/:bug_id/comment', apiController.createComment)//Create single comment
+router.get('/project/:project_id/bug/:bug_id/comment/:comment_id', apiController.getSingleComment)
+//Update Single comment
+//Delete Single comment
 
 module.exports = router
